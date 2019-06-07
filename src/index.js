e56d8cf29efe12df17bc0e6b2841a53482ac85c4
Mohamed Elbaz <albaz2000eg@gmail.com>
2019-06-07 18:13:49 +0200

    replace deprecate apollo-client-preset with apollo-boost

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo-hooks';
-import { HttpLink, ApolloClient, InMemoryCache } from 'apollo-client-preset';
+import { HttpLink, ApolloClient, InMemoryCache } from 'apollo-boost';
 
 import App from './app';
 import serverUri from './app/constants';
