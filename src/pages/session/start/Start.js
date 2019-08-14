2af84eefef8a8f1d03e4ae67b1aaeb2caa0ea7df
bazokhan <albaz2000eg@gmail.com>
2019-08-14 11:48:46 +0200

    change session status

--- src/pages/session/start/Start.js
+++ src/pages/session/start/Start.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 import Main from 'layout/Main';
 import { Link, CircularProgress, Button } from '@material-ui/core';
-import { useQuery } from 'react-apollo';
+import { useQuery, useMutation } from 'react-apollo';
 import Question from '../components/Question';
 import Choice from '../components/Choice';
 import sessionByIDGql from '../gql/sessionByID.gql';
+import changeStatusGql from './gql/changeStatus.gql';
 
 const Start = ({ match }) => {
   const [question, setQuestion] = useState(null);
@@ -16,6 +17,20 @@ const Start = ({ match }) => {
     variables: { publicId: match.params.sessionId }
   });
   const { sessionByID: session, error, loading } = data;
+
+  const [changeStatusMutation] = useMutation(changeStatusGql);
+  const pauseSession = () =>
+    changeStatusMutation({
+      variables: { publicId: match.params.sessionId, status: 'PAUSED' }
+    });
+  const endSession = () =>
+    changeStatusMutation({
+      variables: { publicId: match.params.sessionId, status: 'ENDED' }
+    });
+  const startSession = () =>
+    changeStatusMutation({
+      variables: { publicId: match.params.sessionId, status: 'ACTIVE' }
+    });
 
   useEffect(() => {
     if (session && session.activeQuestion) {
@@ -62,23 +77,55 @@ const Start = ({ match }) => {
     <Main>
       <div>Sessions ID: {session.publicId}</div>
       <div>{session.name}</div>
-      <Question
-        label={`Question ${questionIndex + 1} : `}
-        key={question.id}
-        question={question}
-      >
-        <Button onClick={() => handleNavClick(-1)}>Previous</Button>
-        <Button onClick={() => handleNavClick(1)}>Next</Button>
-        {question.choices.map((choice, choiceIndex) => (
-          <Choice
-            label={`${choiceIndex + 1} - `}
-            key={choice.id}
-            choice={choice}
-          />
-        ))}
-      </Question>
+      <div>{session.status}</div>
+      {session.status === 'ACTIVE' && (
+        <>
+          <Question
+            label={`Question ${questionIndex + 1} : `}
+            key={question.id}
+            question={question}
+          >
+            <Button onClick={() => handleNavClick(-1)}>Previous</Button>
+            <Button onClick={() => handleNavClick(1)}>Next</Button>
+            {question.choices.map((choice, choiceIndex) => (
+              <Choice
+                label={`${choiceIndex + 1} - `}
+                key={choice.id}
+                choice={choice}
+              />
+            ))}
+          </Question>
+          <button type="button" onClick={pauseSession}>
+            Pause
+          </button>
+          <button type="button" onClick={endSession}>
+            Stop
+          </button>
+        </>
+      )}
+      {session.status === 'PAUSED' && (
+        <>
+          <button type="button" onClick={startSession}>
+            Resume
+          </button>
+          <button type="button" onClick={endSession}>
+            Stop
+          </button>
+        </>
+      )}
+      {session.status === 'ENDED' && (
+        <button type="button" onClick={startSession}>
+          Start
+        </button>
+      )}
+      {session.status === 'PENDING' && (
+        <button type="button" onClick={startSession}>
+          Start
+        </button>
+      )}
+
       <Link to={`/sessions/${session.publicId}`} component={RouterLink}>
-        Stop This Session
+        Return to session overview
       </Link>
     </Main>
   );
